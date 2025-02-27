a9f8e3703b030285138c7ea136f0b61df3e2a2b0
liyilong <mondaycha@outlook.com>
2025-02-27 15:59:06 +0800

    ci: use root base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
   favicon: "img/logo.svg",
 
   // Set the production url of your site here
-  url: "https://gpu.act.buaa.edu.cn",
+  url: "https://raids-lab.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/website/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
